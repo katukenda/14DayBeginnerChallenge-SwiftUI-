3be8c59aca6676b9c55d9cd15f91e0b036ec4d4e
katukenda <katukenda@icloud.com>
2021-04-22 07:51:04 +0530

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Janitha Katukenda on 2021-04-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Janitha Katukenda on 2021-04-22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
