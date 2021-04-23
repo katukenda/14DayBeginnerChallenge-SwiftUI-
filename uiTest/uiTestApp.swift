222d92d897b2cf0c8f8ad1b2dce89e072f431a6c
katukenda <katukenda@icloud.com>
2021-04-23 07:09:21 +0530

    Initial Commit

---
+++ uiTest/uiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiTestApp.swift
+//  uiTest
+//
+//  Created by Janitha Katukenda on 2021-04-23.
+//
+
+import SwiftUI
+
+@main
+struct uiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
