83b64f9d6a0badedd6092139639787e30dde7b0c
katukenda <katukenda@icloud.com>
2021-04-23 09:07:24 +0530

    challenge one

--- uiTest/ContentView.swift
+++ uiTest/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, World")
+            .padding(.all)
+            .background(Color.green)
+            .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            .padding(.all, 21.0)
+            .background(Color.blue)
+            .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+
+            
+     
+
     }
 }
+      
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+   
         ContentView()
+            .preferredColorScheme(.light)
+            
+          
     }
 }
